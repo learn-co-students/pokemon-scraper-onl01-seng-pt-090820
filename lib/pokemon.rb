41b754d26392a35149074bb2f738026abfb019f2
AnaBetancourt <anaa.betancourt13@gmail.com>
2020-11-10 03:01:32 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def save
+        
+    end
+
+
+
 end
