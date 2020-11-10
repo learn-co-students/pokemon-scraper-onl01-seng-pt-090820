2bd70e0b3998e33f2fed3c5121ce1d86a5dc7a3e
DavidRMorphew <morphewd@umich.edu>
2020-11-09 22:17:07 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(hash)
+        hash.each {|k,v| self.send(("#{k}="),v)}
+    end
+
+    # Why is this a class method?
+    def self.save(name, type, db)
+        @db = db
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        @db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        @db = db
+        sql = <<-SQL
+            SELECT * FROM pokemon WHERE id = ?
+        SQL
+        pokemon_array = @db.execute(sql, id).flatten
+        hash = {}
+        hash[:id] = pokemon_array[0]
+        hash[:name] = pokemon_array[1]
+        hash[:type] = pokemon_array[2]
+        hash[:db] = db
+        # binding.pry
+        new_pokemon = self.new(hash)
+    end
 end
